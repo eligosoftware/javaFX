e7ba19b1a9a5cb7824516acd641349aa958fdd71
Ilqar Rasulov <gragli98@gmail.com>
2017-08-01 15:22:03 +0400

    Finished with controls

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,11 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 
@@ -15,6 +18,16 @@ public class Controller {
     private Button hello_button;
     @FXML
     private Button bye_button;
+    @FXML
+    private CheckBox ourCheckBox;
+    @FXML
+    private Label ourLabel;
+
+    @FXML
+    public void handleChange()
+    {
+        System.out.println("The checkbox is "+(ourCheckBox.isSelected()?"checked":"not checked"));
+    }
 
     @FXML
     public void initialize(){
@@ -34,6 +47,35 @@ public class Controller {
         }
         else{
             System.out.println("Dunno what to say");
+        }
+
+        Runnable task=new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    String s=Platform.isFxApplicationThread()?"UI Thread":"Background Thread";
+                    System.out.println("I'm going to sleep on the: "+s);
+                    Thread.sleep(10000);
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            String s=Platform.isFxApplicationThread()?"UI Thread":"Background Thread";
+                            System.out.println("I'm updating the label on the: "+s);
+                            ourLabel.setText("We did something");
+                        }
+                    });
+
+                } catch (InterruptedException event){
+                    //we don't care about that right now
+                }
+            }
+        };
+
+        new Thread(task).start();
+        if(ourCheckBox.isSelected()){
+            name_text.clear();
+            hello_button.setDisable(true);
+            bye_button.setDisable(true);
         }
     }
     @FXML
